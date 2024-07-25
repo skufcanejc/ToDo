1537328b4320044125d2af69fddd2d87200fc5da
Nejc Škufca <nejc@Nejcs-MacBook-Air.local>
2024-07-25 09:23:32 +0200

    implemented the complete and exit buttons

--- gui.py
+++ gui.py
@@ -7,9 +7,14 @@ add_button = sg.Button("Add")
 list_box = sg.Listbox(values=functions.get_todos(), key = 'todos_list',
                       enable_events=True, size=[45, 10])
 edit_button = sg.Button("Edit")
+complete_button = sg.Button("Complete")
+exit_button = sg.Button("Exit")
 
-window = sg.Window("My To-Do app", layout=[[label], [input_box], [add_button], [list_box, edit_button]],
-                   font=("Helvetica", 16))
+window = sg.Window("My To-Do app", layout=[[label], 
+                                           [input_box, add_button],
+                                           [list_box, edit_button, complete_button],
+                                           [exit_button]],
+                                    font=("Helvetica", 16))
 
 
 while True:
@@ -33,6 +38,15 @@ while True:
             todos[index] = new_todo
             functions.write_todos(todos)
             window['todos_list'].update(values=todos)
+        case "Complete":
+            todo_to_complete = values['todos_list'][0]
+            todos = functions.get_todos()
+            todos.remove(todo_to_complete)
+            functions.write_todos(todos)          
+            window['todos_list'].update(values=todos)
+            window['todo'].update(value='')  
+        case "Exit":
+            break
         case "todos_list":
             window['todo'].update(value=values['todos_list'][0])
         case sg.WIN_CLOSED:
